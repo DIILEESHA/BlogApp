422d48b9ed5d8db5d12921cc375f9156719d2506
Dileesha Lakshan <89340276+DIILEESHA@users.noreply.github.com>
2022-07-13 18:30:37 +0530

    create models

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,29 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      required: true,
+      type: String,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("post", postSchema);
